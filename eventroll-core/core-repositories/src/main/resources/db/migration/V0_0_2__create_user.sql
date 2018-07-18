ee4628ec9740ff952d50ee25fbc2e56e22448eb1
William Arustamyan <williamarustamyan@gmail.com>
2018-07-18 08:47:56 +0400

    Added eventroll application user

---
+++ eventroll-core/core-repositories/src/main/resources/db/migration/V0_0_2__create_user.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE seq_user START 1 INCREMENT 50;
+
+
+CREATE TABLE t_user (
+  id         BIGINT       NOT NULL CONSTRAINT pk_user PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name  VARCHAR(100) NOT NULL,
+  user_name  VARCHAR(100) NOT NULL,
+  password   VARCHAR(100) NOT NULL,
+  enabled    BOOLEAN      NOT NULL,
+  created    TIMESTAMP    NOT NULL,
+  updated    TIMESTAMP    NOT NULL,
+  deleted    TIMESTAMP
+);
